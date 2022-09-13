704e4b7a524c2bb100a42b57067941079b068935
Chloe ES <37400078+chloeraina95@users.noreply.github.com>
2022-09-13 13:33:00 +0800

    update chrome logo and shout icon and css

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// banner
+loadSlider();
+function loadSlider(){
+    $('.general_banner').slick({
+      infinite: true,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: true,
+    });
+  }
+  function callbanner(x){
+    swal({
+        title: "Sample Alert",
+        text: "You clicked the banner",
+        icon: "error",
+        button: "OK",
+    });
+  }
